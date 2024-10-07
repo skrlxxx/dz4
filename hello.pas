de0d161b4a2f7ae3a7fef503ec266259f8c13688
skrlxxx <141816920+skrlxxx@users.noreply.github.com>
2024-10-07 21:43:01 +0300

    Update hello.pas

--- hello.pas
+++ hello.pas
@@ -1,3 +1,9 @@
 ﻿begin
-  println('Привет, мир!');
+  var hello:= readstring('Введите язык, на котором вы говорите, Enter the language, Sprache eingeben');
+  if hello = ('русский язык') then
+    println('Привет');
+  if hello = ('English') then
+    println('Hello');
+  if hello = ('Deutsch') then
+    println('Hallo');
 end.
